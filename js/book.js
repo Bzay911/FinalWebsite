32be40f7691488f7f11d964c1b54683071e4b184
SumitGhale <grgsten@gmail.com>
2023-08-06 19:28:20 +1000

    Added by sumit

--- js/book.js
+++ js/book.js
@@ -1,28 +1,33 @@
-let rooms = [
-    {
-        id:1,
-        heading:"Spacious room, perfect for a family",
-        features:"2 beds, 1 bathroom, 1 balcony",
-        host:"Saurab Khanal",
-        time: "Posted: 1 week ago",
-        img:"./images/room1.jpg"
-    },
-    {
-        id:2,
-        heading:"Spacious room, perfect for a family",
-        features:"2 beds, 1 bathroom, 1 balcony",
-        host:"Saurab Khanal",
-        time: "Posted: 1 week ago",
-        img:"./images/room2.jpg"
+// let rooms = [
+//     {
+//         id:1,
+//         heading:"Spacious room, perfect for a family",
+//         features:"2 beds, 1 bathroom, 1 balcony",
+//         host:"Saurab Khanal",
+//         time: "Posted: 1 week ago",
+//         img:"./images/room1.jpg"
+//     },
+//     {
+//         id:2,
+//         heading:"Spacious room, perfect for a family",
+//         features:"2 beds, 1 bathroom, 1 balcony",
+//         host:"Saurab Khanal",
+//         time: "Posted: 1 week ago",
+//         img:"./images/room2.jpg"
 
-    }
-]
+//     }
+// ]
 
-let bookedRooms = []
+// let bookedRooms = []
 
-const btn = document.querySelector(".button_room")
-    btn.addEventListener('click',()=>{
-        let room_id = btn.getAttribute("data-id")
-        bookedRooms.push(room_id)
-        localStorage.setItem('booked',JSON.parse(bookedRooms))
-})
+// const btn = document.querySelector(".button_room")
+//     btn.addEventListener('click',()=>{
+//         let room_id = btn.getAttribute("data-id")
+//         bookedRooms.push(room_id)
+//         localStorage.setItem('booked',JSON.parse(bookedRooms))
+// })
+
+const bookingButton = document.getElementById('availabilityButotn');
+bookingButton.addEventListener('click', () => {
+    window.location.href="/homelist.html"
+});
